f0cfdaa2dfa652cbf048a550a9d37cb8c4d83a17
zhaoxb <13486627323@163.com>
2021-09-29 18:53:11 +0800

    保证可见性

--- redisson/src/main/java/net/zhaoxiaobin/redisson/service/DistributionLockDemo.java
+++ redisson/src/main/java/net/zhaoxiaobin/redisson/service/DistributionLockDemo.java
@@ -14,7 +14,7 @@ public class DistributionLockDemo {
     /**
      * 库存
      */
-    public static int count = 20;
+    public static volatile int count = 20;
 
     @DistributionLock(value = "incrementLock", waitTime = 1000)
     public boolean increment() {
